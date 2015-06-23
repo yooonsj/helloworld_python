11906109711175227b1c2f170386b947aeb68343
YoonSangjun <yooonsj@gmail.com>
2015-06-23 10:49:27 +0900

    주석

--- comment/ex01.py
+++ comment/ex01.py
@@ -6,10 +6,10 @@ egoing
 # user input password
 input = 33
 real_egoing = 11
-#real_k8805 = 'ab'
+# real_k8805 = 'ab'
 if real_egoing == input:
     print('Hello!, egoing')
-#elif real_k8805 == input:
+# elif real_k8805 == input:
 #    print('Hello!, k8805')
 else:
     print('Who are you?')
