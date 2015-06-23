758296844e46e1939b19357dc737f634aa6deb60
YoonSangjun <yooonsj@gmail.com>
2015-06-23 13:40:59 +0900

    반복문

---
+++ loop/ex01.py
@@ -0,0 +1,9 @@
+while False:
+    print('Hello world!')
+print('After while')
+
+# 반복문 조건
+i = 0;
+while i < 3:
+    print('Hello world')
+    i = i + 1
---
+++ loop/ex03.py
@@ -0,0 +1,14 @@
+i = 0
+while i < 10:
+    if i == 4:
+        print(i)
+    i = i + 1
+
+print('==================')
+i = 0
+while i < 10:
+    if i == 4:
+        break
+    print(i)
+    i = i + 1
+print('after while')
